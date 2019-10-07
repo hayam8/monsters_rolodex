7f0fff47bfcf254e449428ba37dec6435eb1b745
hayam8 <hayam8@uw.edu>
2019-10-07 11:36:29 -0700

    Updates

---
+++ precache-manifest.091d544b7e32046e033460b005006a9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca0ba9bc19e3bdba04fab2d988a028a",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "10533dc6c81bede7d6d7",
+    "url": "/monsters_rolodex/static/css/main.8052fc48.chunk.css"
+  },
+  {
+    "revision": "ca903edc950d52328219",
+    "url": "/monsters_rolodex/static/js/2.e124cbd2.chunk.js"
+  },
+  {
+    "revision": "10533dc6c81bede7d6d7",
+    "url": "/monsters_rolodex/static/js/main.934fe5b7.chunk.js"
+  },
+  {
+    "revision": "d4a0b6edee43a41c0e5c",
+    "url": "/monsters_rolodex/static/js/runtime-main.165b81c4.js"
+  }
+]);
